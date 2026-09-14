6d178aac2d5d2df9b739213518d58f65b1c8f724
Pirashaliny Arinchan <arinchanpirasha@gmail.com>
2026-09-14 16:26:16 +0530

    Update db.js

--- task-02/backend/src/db.js
+++ task-02/backend/src/db.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-const mysql = require('mysql2/promise');
-const { getDbConfig } = require('./config');
-
-const pool = mysql.createPool(getDbConfig());
-=======
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
@@ -12,12 +6,11 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'techloom_pos',
+  database: process.env.DB_NAME || 'techloom_ecommerce',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   decimalNumbers: true
 });
->>>>>>> c8e52bbd3c97e09a7f2cfafcb9e9f9787748df3b
 
 module.exports = pool;
